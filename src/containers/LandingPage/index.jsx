c2b66b580e3a2bbc5091cbc1b2e849ebae61599f
Yoelvis Mulen <ymulenll@gmail.com>
2020-02-07 16:59:34 -0300

    undo use useCallback hook to avoid unnecessary renders

--- src/containers/LandingPage/index.jsx
+++ src/containers/LandingPage/index.jsx
@@ -1,4 +1,4 @@
-import React, { useState, memo, useEffect, useCallback } from 'react';
+import React, { useState, memo, useEffect } from 'react';
 import { get } from '../../utils/apiClient';
 import Search from '../../components/Search';
 import Rating from '../../components/Rating';
@@ -17,12 +17,10 @@ const LandingPage = () => {
   const [debouncing, setDebouncing] = useState(false);
   const debouncedSearch = useDebounce(search, 300);
 
-  const onChangeStars = useCallback(() => {
-    return starPosition => {
-      const newStars = starPosition === stars ? 0 : starPosition;
-      setStars(newStars);
-    };
-  }, [stars]);
+  const onChangeStars = starPosition => {
+    const newStars = starPosition === stars ? 0 : starPosition;
+    setStars(newStars);
+  };
 
   const onSetSearch = newSearch => {
     setPage(1);

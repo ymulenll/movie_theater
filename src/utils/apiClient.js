750273e165e1312884748ce6a523cc0561b7793c
Yoelvis Mulen <ymulenll@gmail.com>
2020-05-12 21:34:29 -0300

    apiClient improvement

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,17 +1,26 @@
 const headers = new Headers();
 
-headers.append('Accept', 'application/json');
+headers.append("Accept", "application/json");
 headers.append(
-  'Authorization',
-  'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs',
+  "Authorization",
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1NzUzN2ZmMTlmMzgxZGQ3YjY3ZWVlMWVhOGI4MTY0YSIsInN1YiI6IjVlM2ExNmU1MGMyNzEwMDAxODc1NTI4MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.nOpZ_nBtA93tbzr6-rxD0760tssAAaSppyjRv9anArs"
 );
 
 const requestOptions = {
-  method: 'GET',
+  method: "GET",
   headers,
 };
 
-const API_URL = 'https://api.themoviedb.org/3/';
+const API_URL = "https://api.themoviedb.org/3";
 
-export const get = url =>
-  fetch(`${API_URL}${url}`, requestOptions).then(result => result.json());
+export const get = (endpoint) =>
+  fetch(`${API_URL}/${endpoint}`, requestOptions).then(
+    async (response) => {
+      const data = await response.json();
+      if (response.ok) {
+        return data;
+      } else {
+        return Promise.reject(data);
+      }
+    }
+  );
